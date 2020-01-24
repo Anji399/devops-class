273784b1069787813751614ff513a8d8ef065e69
Anji399 <mvpareddy3493@gmail.com>
2020-01-24 15:04:19 +0530

    third java program

---
+++ string.java
@@ -0,0 +1,13 @@
+class string
+{
+  public static void main(String args[])
+   {
+
+
+      String str = "Prasanna";
+      for(int i=0; i<str.length();i++){
+        char ch = str.charAt(i);
+        System.out.println("character at "+i+" position: "+ch);
+      }
+   }
+}
